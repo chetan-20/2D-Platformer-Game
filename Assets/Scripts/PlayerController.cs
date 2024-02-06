7e4e7c5849048a07804c0514bb2c5ec0f1e8e8ad
Chetan Rautela <chetanrautela20@gmail.com>
2024-02-06 14:53:29 +0530

    Added movement
    made character move and jump properly

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,107 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public Animator animator;
+    private SpriteRenderer spriteRenderer;
+    private Rigidbody2D rb; 
+    public float jump = 5f;
+    public float speed=1f;
+    private bool iscrouch=false;
+    private bool isGrounded = true;
+   
+    
+   
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        rb = GetComponent<Rigidbody2D>();         
+        animator.SetBool("IsCrouching", false);
+        animator.SetBool("IsJumping", false);       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horispeed = Input.GetAxisRaw("Horizontal");
+        flipx(horispeed);
+        Jumpy();                   
+        Crouchy();
+        Movement(horispeed);
+    }
+
+    void flipx(float horispeed)
+    {
+        if (horispeed < 0)
+        {
+            spriteRenderer.flipX = true;
+
+        }
+        if (horispeed > 0)
+        {
+            spriteRenderer.flipX = false;
+
+        }
+        animator.SetFloat("Speed", horispeed);
+    }
+
+    void Jumpy()
+    {
+        if ( Input.GetKeyDown(KeyCode.Space)  && isGrounded == true){
+           
+            animator.SetBool("IsJumping", true);
+            animator.SetBool("IsCrouching", false);
+            rb.velocity = new Vector2(rb.velocity.x, jump);
+            isGrounded = false;
+            
+        }
+        else 
+        {
+            animator.SetBool("IsJumping", false);
+            Standing();
+        }
+    }
+
+    void Crouchy()
+    {
+        if (Input.GetKey(KeyCode.LeftControl) && iscrouch==false)
+        {
+            iscrouch = true;
+            animator.SetBool("IsCrouching", true);
+            animator.SetBool("IsJumping", false);          
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftControl) && iscrouch==true)
+        {
+            iscrouch = false;
+            animator.SetBool("IsCrouching", false);
+             Standing();
+        }
+        
+             
+    }
+
+    void Standing()
+    {
+        animator.SetBool("IsCrouching", false);
+        animator.SetBool("IsJumping", false);            
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isGrounded = true;
+           
+        }
+    }
+
+    void Movement(float horispeed)
+    {
+        Vector2 position = transform.position;
+        position.x += horispeed * speed * Time.deltaTime;
+        transform.position = position;
+    }
+
+}
